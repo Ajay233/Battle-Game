e0981889ad9ef16c682bdc1f949ebc6120ea8bd2
Ajay <ajaymungur@hotmail.com>
2018-11-14 15:31:10 +0000

    created web_helpers.rb to refactor tests.

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,10 +1,7 @@
 # Add in within () do .. end when this works
 feature 'testing game setup' do
   scenario 'can add 2 players and display thier names' do
-    visit('/')
-    fill_in('player_1', with: 'Ryu')
-    fill_in('player_2', with: 'Ken')
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Ryu vs Ken .... FIGHT!'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'testing hit points' do
   scenario "can display player 2's hit points" do
-    visit('/')
-    fill_in('player_1', with: 'Ryu')
-    fill_in('player_2', with: 'Ken')
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Ken HP = 100'
   end
 end
