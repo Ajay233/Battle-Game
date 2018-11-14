bc27d8ad0ca716b8b09ab30c45be49c555188640
Ajay <ajaymungur@hotmail.com>
2018-11-14 13:21:09 +0000

    Initialized rspec, capybara and initial app file

---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,7 @@
+
+feature 'testing infrastructures' do
+  scenario 'can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
