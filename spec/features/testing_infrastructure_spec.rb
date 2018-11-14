1503dfa96f13683f21a7dc10b25a2798cf12ad35
Ajay <ajaymungur@hotmail.com>
2018-11-14 14:35:54 +0000

    Added form for entering player 1 and 2 names

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,7 +1,10 @@
-
-feature 'testing infrastructures' do
-  scenario 'can run app and check page content' do
+# Add in within () do .. end when this works
+feature 'testing game setup' do
+  scenario 'can add 2 players and display thier names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in('player_1', with: 'Ryu')
+    fill_in('player_2', with: 'Ken')
+    click_button 'Submit'
+    expect(page).to have_content 'Ryu vs Ken .... FIGHT!'
   end
 end
